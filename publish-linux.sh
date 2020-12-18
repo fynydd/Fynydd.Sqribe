40102d01f1720069ff596e933fe81eeace3776fb
Michael Argentini <michael@argentini.us>
2020-12-18 18:48:59 -0500

    Linux publish now copies an install script

--- publish-linux.sh
+++ publish-linux.sh
@@ -4,3 +4,4 @@ cp -r SQribe/templates SQribe/bin/Release/net5.0/linux-x64/publish/
 cp SQribe/HELP.txt SQribe/bin/Release/net5.0/linux-x64/publish/
 cp SQribe/backup-defaults.json SQribe/bin/Release/net5.0/linux-x64/publish/
 cp SQribe/restore-defaults.json SQribe/bin/Release/net5.0/linux-x64/publish/
+cp linux-install.sh SQribe/bin/Release/net5.0/linux-x64/publish/
